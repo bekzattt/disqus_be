543ff2a90c028f5e94edd7e91e67e94263452e59
bekzattt <bekzat.ktl@gmail.com>
2015-05-04 13:18:46 +0600

    Backend comments

---
+++ disqus_be/core_app/models.py
@@ -0,0 +1,14 @@
+"""This module contains DB models"""
+from django.db import models
+from django.utils import timezone
+from core_app import utils
+
+# create our model Comment
+
+class Comment(models.Model):
+
+    url = models.CharField(max_length=1000, default='')
+    name = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000)
+    comment = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=timezone.now)
